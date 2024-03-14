f73cb837d30f76bda36c8f314069ad9391c900d6
chiedzachimedza <chiedzachimedza@gmail.com>
2024-03-14 20:50:48 +0000

    my first commit

---
+++ file.py
@@ -0,0 +1,21 @@
+#subtraction
+
+# a = 12
+
+# b = 10
+
+# def subtract(a, b):
+#     print(f'The result of subtracting{a, b} is {a - b}' )
+
+# subtract(a,b)
+
+#division
+
+a = 18
+
+b = 9
+
+def divide(a, b):
+    print(f"the result of dividing {a} and {b} is {a / b}")
+
+divide(a, b)
